e4e9608f348f59da4778fbec50826e5ecb91d0cf
Shivam Kanoria <shivam.manu05@gmail.com>
2019-02-27 21:06:32 -0500

    Update GraduationTrackerTests.cs

--- GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -70,6 +70,10 @@ namespace GraduationTracker.Tests.Unit
 
             //tracker.HasGraduated()
         };
+            var diploma = Repository.GetDiploma(1);
+            Student[] students = { Repository.GetStudent(1), Repository.GetStudent(2), Repository.GetStudent(3), Repository.GetStudent(4) };
+
+            
             
             var graduated = new List<Tuple<bool, STANDING>>();
 
@@ -78,11 +82,34 @@ namespace GraduationTracker.Tests.Unit
                 graduated.Add(tracker.HasGraduated(diploma, student));      
             }
 
-            
-            Assert.IsFalse(graduated.Any());
+            Assert.AreEqual(2, graduated.Where(g => !g.Item1 && g.Item2.Equals(STANDING.Remedial)).Count()); // only two students must have failed
+            Assert.IsTrue(graduated[2].Item2.Equals(STANDING.SumaCumLaude) && graduated[3].Item2.Equals(STANDING.Average));
 
         }
 
+        [TestMethod]
+        public void TestStudentHasOptedForAllRequiredCourses()
+        {
+            var tracker = new GraduationTracker();
+            var diploma = Repository.GetDiploma(2);
+            Student student = Repository.GetStudent(5);
 
+            Tuple<bool, STANDING> result = tracker.HasGraduated(diploma, student);
+            Assert.AreEqual(new Tuple<bool, STANDING>(false, STANDING.Remedial), result);
+
+        }
+
+        [TestMethod]
+        public void TestStudentHasOptedForAtLeastOneCourses()
+        {
+            var tracker = new GraduationTracker();
+            var diploma = Repository.GetDiploma(2);
+            Student student = Repository.GetStudent(5);
+
+            Tuple<bool, STANDING> result = tracker.HasGraduated(diploma, student);
+            Assert.AreEqual(new Tuple<bool, STANDING>(false, STANDING.Remedial), result);
+        }
+        
+        
     }
 }
